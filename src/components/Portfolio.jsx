064b661ab7881261492e485907c60f96b5d066d7
Mohammad Rezvani <mohammadrezvani2002@gmail.com>
2023-02-17 16:51:45 +0330

    PortFolio Changed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,36 +1,43 @@
 import React from "react";
-import arrayDestruct from "../assets/portfolio/arrayDestruct.jpg";
+
 import installNode from "../assets/portfolio/installNode.jpg";
 import navbar from "../assets/portfolio/navbar.jpg";
 import reactParallax from "../assets/portfolio/reactParallax.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
+import todo from "../assets/portfolio/todo.jpg";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: arrayDestruct,
+      src: todo,
+      title: "Todo App",
     },
     {
       id: 2,
       src: reactParallax,
+      title: "Tailwind css",
     },
     {
       id: 3,
       src: navbar,
+      title: "Hello",
     },
     {
       id: 4,
       src: reactSmooth,
+      title: "Test",
     },
     {
       id: 5,
       src: installNode,
+      title: "Yes sir",
     },
     {
       id: 6,
       src: reactWeather,
+      title: "Im Goat",
     },
   ];
 
@@ -50,18 +57,21 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, title }) => (
             <div key={id} className=" shadow-md shadow-gray-800 rounded-lg">
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
+              <p className="bg-purple-500 text-center rounded-sm mt-2 py-1 px-3 text-white text-lg">
+                {title}
+              </p>
               <div className="flex items-center justify-center">
-                <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-white ">
+                <button className=" w-1/2 px-6  m-4 duration-200 hover:scale-105 text-white ">
                   Demo
                 </button>
-                <button className="px-6 py-3 w-1/2 m-4 duration-200 hover:scale-105 text-white">
+                <button className="px-6  w-1/2 m-4 duration-200 hover:scale-105 text-white">
                   Code
                 </button>
               </div>
